9824086ab214ef1afe4bb3c49bfe851f66a152c2
Chris Paul <cmpaul@gmail.com>
2017-08-13 17:47:17 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello, world!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('Something broke!', err)
+  }
+  console.log(`Listening on ${port}`)
+})
