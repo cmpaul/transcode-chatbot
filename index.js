5536309e94758913289664ef4e7e4ed71cf8a205
Chris Paul <cmpaul@gmail.com>
2017-08-13 21:08:35 -0700

    Add Slack response

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const http = require("http");
 const port = 3000;
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end("Hello, world!");
+  response.end(
+    JSON.stringify({
+      text: "Hello, world!"
+    })
+  );
 };
 
 const server = http.createServer(requestHandler);
