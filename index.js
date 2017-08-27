fb32b04fbae2067bfe916a822e29d955a99cb154
Chris Paul <cmpaul@gmail.com>
2017-08-27 14:17:18 -0700

    Improve readability and comments

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ const http = require("http");
 const qstr = require('querystring');
 const port = 3000;
 
-const requestHandler = (request, response) => {
+// Create the function that will respond to a request
+const requestHandler = function(request, response) {
+
+  // We only care about POST requests
   if (request.method == "POST") {
+
+    // Read the POST body payload
     let body = "";
     request.on("data", (data) => {
       body += data;
       if (body.length > 1e6) request.connection.destroy();
     });
+
+    // When we're done reading the payload, respond
     request.on("end", () => {
       let post = qstr.parse(body);
       console.log(post);
@@ -24,7 +31,7 @@ const requestHandler = (request, response) => {
 
 const server = http.createServer(requestHandler);
 
-server.listen(port, err => {
+server.listen(port, function(err) {
   if (err) {
     return console.log("Something broke!", err);
   }
